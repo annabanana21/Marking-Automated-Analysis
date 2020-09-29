71639942fab19557353c996e5e569fc822dd38a3
Anna <av.mastoris@gmail.com>
2020-09-28 21:33:01 -0400

    Marking recommendation completed version 1

--- client/src/functions/markSetter.js
+++ client/src/functions/markSetter.js
@@ -14,15 +14,20 @@ function markSetter(userMap, pulls, tickets) {
 
         //Create a stats value representing their overall score
         let rank = 0;
+        let recommendations = [];
 
         if (value.pullRequests && value.tickets) {
             //Check if descriptive and grouped pulls to tickets
-            rank += markGetter(value.pullRequests.length, value.tickets.length);
+            let result = markGetter(value.pullRequests.length, value.tickets.length, [3.5, 3, 1, 0.5]);
+            if (result <= 1) {
+                recommendations.push('Remember to open a pull request for each feature!')
+            }
+            rank += result;
         }
 
         if (value.tickets) {
             let amount = (tickets.length-4)/amountWithTickets;
-            rank+= markGetter(value.tickets.length, amount)
+            rank+= markGetter(value.tickets.length, amount, [3.5, 3, 1, 0.5])
         }
 
         if (value.comments || value.reviewComments) {
@@ -31,17 +36,34 @@ function markSetter(userMap, pulls, tickets) {
             value.comments ? comments = value.comments.length : comments = 0;
             value.reviewComments ? review = value.reviewComments.length : review = 0;
             if (value.pullRequests) {
-                rank+= percentGetter(comments+review, pulls.length-value.pullRequests.length)
+                let result= percentGetter(comments+review, pulls.length-value.pullRequests.length, [3, 2, 1, 0.5]);
+                if (result <= 1) {
+                    recommendations.push("Try to be more active on GitHub. You can directly view the changes made by a group member and create review comments to help them fix bugs.")
+                }
+                rank+= result;
             }
         }
 
-        if (value.ticketComments) {
+
+            // Based on number of tickets commented on receive score
+
+            //Checks for the absence of ticket comments ot pulls
+            let ticketNumber;
+            value.ticketComments ? ticketNumber = value.ticketComments.length : ticketNumber = 0;
             let personPulls;
             value.pullRequests ? personPulls = value.pullRequests.length : personPulls = 0;
-            rank+= percentGetter(value.ticketComments.length, tickets.length-4-personPulls)
-        }
 
-        console.log(key, " ", rank)
+            // result represents score after checking for the percentage of tickets commented on
+            let result = percentGetter(ticketNumber, tickets.length-4-personPulls, [3, 2, 1, 0.5]);
+            if (result <= 1) {
+                recommendations.push("Remember to comment on Jira tickets before they are moved into 'Done'");
+            }
+
+            rank+= result;
+
+
+        value["ranking"] = rank
+        value['rec'] = recommendations
         
 
     }
@@ -62,13 +84,13 @@ function ticketers(map) {
 function percentGetter(amount, compare, pointList) {
     let points = 0;
     if(amount >= compare*0.75) {
-        points = 3;
+        points = pointList[0];
     } else if (amount === compare*0.60) {
-        points = 2;
+        points = pointList[1];
     } else if (amount >= compare*0.25) {
-        points = 1;
+        points = pointList[2];
     } else if (amount > 0) {
-        points = 0.5
+        points = pointList[3]
     } 
     return points;
 }
@@ -76,13 +98,13 @@ function percentGetter(amount, compare, pointList) {
 function markGetter(amount, compare, pointList) {
     let points = 0;
     if(amount >= compare) {
-        points = 3;
+        points = pointList[0];
     } else if (amount === compare-1) {
-        points = 2;
+        points = pointList[1];
     } else if (amount >= compare-3) {
-        points = 1;
+        points = pointList[2];
     } else if (amount > 0) {
-        points = 0.5
+        points = pointList[3]
     } 
     return points;
 }
