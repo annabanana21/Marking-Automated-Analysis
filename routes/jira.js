be3e201cb66017a59d4e0144c3cb5db88dbb7570
Anna <av.mastoris@gmail.com>
2020-09-30 14:11:55 -0400

    Redirect value added to env variables

--- routes/jira.js
+++ routes/jira.js
@@ -16,7 +16,7 @@ router.get('/auth', async (req, res) => {
     
     let bound = crypto.createHash('sha256').update(uuid.v1()).update(crypto.randomBytes(256)).digest("hex");
     
-    res.redirect(`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${process.env.REACT_APP_JIRA_CLIENT}&scope=read%3Ajira-user%20read%3Ajira-work%20manage%3Ajira-project&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fjira&state=${bound}&response_type=code&prompt=consent`)
+    res.redirect(`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${process.env.REACT_APP_JIRA_CLIENT}&scope=read%3Ajira-user%20read%3Ajira-work%20manage%3Ajira-project&redirect_uri=${process.env.REACT_APP_ALT_URI}&state=${bound}&response_type=code&prompt=consent`)
    
 })
 
